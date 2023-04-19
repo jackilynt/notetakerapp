bae916d1f15279a3c88cacd6e739569a1437399b
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-04-19 15:36:42 -0400

    changes to apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { v4: uuidv4 } = require("uuid");
-
+const fs = require("fs");
 const router = Router();
 
 // your api routes
@@ -8,9 +8,10 @@ router.get("/notes", (req, res) => {
   // what you could return, replace the  <{ message: "hit" }> with the actual data from db.json
   fs.readFromFile("./notes").then((data) => res.json(JSON.parse(data)));
 
-  return res.status(200).json({ message: "hit" });
+  res.status(200).json(`${req.method} request received to get new notes`);
 });
 router.post("/notes", (req, res) => {
+  console.info(`${req.method} request received to add a new note`);
   // building the new note
   const newNote = {
     id: uuidv4(),
